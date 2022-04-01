5b88b5df9eecce669d5f27b3c0fdaf699030a1b2
marty <marty@geyst.ch>
2022-04-01 08:22:47 +0200

    add navOpen to body

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,12 +4,21 @@
 
 <script>
 export default {
+  computed: {
+    navOpen() {
+      return this.$store.state.navigation.navOpen
+    },
+  },
   watch: {
     // watch for route change
     $route() {
       // set navigation toggle to false (close all navigation)
       this.$store.commit('navigation/toggle', false)
     },
+    navOpen(store) {
+      // console.log("watching navOpen", store)
+      document.querySelector('body').classList.toggle('navOpen')
+    }
   },
 }
 </script>
