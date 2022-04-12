ea187103b616498e160c35d58c34575a8758cd9d
Dollique <hello@tobiasmarty.ch>
2022-04-12 22:34:57 +0200

    try to render components in rich text (WIP)

--- components/content/MyTitle.vue
+++ components/content/MyTitle.vue
@@ -7,7 +7,6 @@ export default {
   props: {
     blok: {
       type: Object,
-
       required: true,
     },
   },
--- plugins/storyblok-rich-text-renderer.js
+++ plugins/storyblok-rich-text-renderer.js
@@ -2,3 +2,12 @@ import Vue from 'vue'
 import VueRichTextRenderer from '@marvr/storyblok-rich-text-vue-renderer'
 
 Vue.use(VueRichTextRenderer)
+/*import Teaser from '~/components/content/MyTitle.vue'
+
+Vue.use(VueRichTextRenderer, {
+  resolvers: {
+    components: {
+      mytitle: Teaser
+    }
+  }
+});*/
