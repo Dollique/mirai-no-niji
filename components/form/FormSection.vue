8b81e20ff3a099bef75abeae0bfb3afa335eb62c
marty <marty@geyst.ch>
2022-04-13 19:09:47 +0200

    trying out form action

--- components/form/FormSection.vue
+++ components/form/FormSection.vue
@@ -5,7 +5,7 @@
     </fieldset>
 
     <fieldset>
-      <button type="submit">Submit</button>
+      <button type="submit" class="form__button" data-callback="handleFormSubmit">Submit</button>
     </fieldset>
   </form>
 </template>
@@ -18,5 +18,45 @@
         required: true,
       },
     },
+  }
+
+  var handleFormSubmit = function () {
+    var formApiEndpoint = 'http://geyst.appengine.flow.ch:8080'
+
+    console.log("test");
+
+    /*
+
+    var formRequest = new Request(formApiEndpoint, {
+        method: 'POST',
+        body: JSON.stringify({
+          name: nameInput.value,
+          message: messageInput.value
+        })
+      })
+
+      fetch(formRequest)
+        .then(function(response) {
+          if (response.status === 200) {
+            return response.json()
+          } else {
+            throw new Error('Something went wrong on api server!')
+          }
+        })
+        .then(function(response) {
+          successEl.style.display = 'block'
+          buttonEl.style.display = 'block'
+          sendingEl.style.display = 'none'
+          nameInput.value = ''
+          messageInput.value = ''
+        }).catch(function(error) {
+        errorsEl.style.display = 'block'
+        buttonEl.style.display = 'block'
+        sendingEl.style.display = 'none'
+        console.error(error)
+      })
+
+    */
+
   }
 </script>
